71eb9b9c5edd8c29532617c43f5cc86406515648
csmsqj <3375160014@qq.com>
2026-06-24 14:58:11 +0800

    rag

--- rag/core/rag_evaluate.py
+++ rag/core/rag_evaluate.py
@@ -2,3 +2,8 @@
 import pandas as pd
 # pandas：数据分析库，用于把评估结果转成 DataFrame 保存为 CSV 文件
 import json
+#"D:\eduEag笔记\EduRAG评估系统笔记7.md"
+#context_relevancy（上下文相关性）
+#context_recall（上下文召回率）
+#faithfulness（忠实度）
+#answer_relevancy（答案相关性）
